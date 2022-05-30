c83a52b168447d8a5b0e56c01b7b345b70e4156e
redotomi <tomy.redondo001@gmail.com>
2022-05-30 17:07:44 -0300

    Agrega ESLint y formatea el código

--- src/main.js
+++ src/main.js
@@ -5,46 +5,41 @@ let anteriorURL;
 async function traerPokemons(url) {
   try {
     const pokemons = await fetch(url);
-    return pokemons.json();;
-  } catch(error) {
+    return pokemons.json();
+  } catch (error) {
     throw new Error(error);
   }
 }
 
-
 async function llenarCartas(cartas) {
   try {
     const dataPokemons = await cartas;
-  const pokemons = dataPokemons.results;
-  
-  pokemons.forEach(pokemon => {
-    const $body = document.createElement('div')
-    $body.textContent = `${pokemon.name.capitalize()}`;
-    
-    crearTarjeta($body, pokemon.name);
-    añadirInteraccion();
-  });
+    const pokemons = dataPokemons.results;
 
-  siguienteURL = dataPokemons.next;
-  anteriorURL = dataPokemons.previous;
+    pokemons.forEach((pokemon) => {
+      const $body = document.createElement('div');
+      $body.textContent = `${pokemon.name.capitalize()}`;
 
-  if(anteriorURL === null){
-    document.querySelector('.anterior').classList.add('oculto');
-  }
-  else {
-    document.querySelector('.anterior').classList.remove('oculto');
-  }
-  if(siguienteURL === null){
-    document.querySelector('.siguiente').classList.add('oculto');
-  }
-  else {
-    document.querySelector('.siguiente').classList.remove('oculto');
-  }
-  
-  }catch(error){
+      crearTarjeta($body, pokemon.name);
+      añadirInteraccion();
+    });
+
+    siguienteURL = dataPokemons.next;
+    anteriorURL = dataPokemons.previous;
+
+    if (anteriorURL === null) {
+      document.querySelector('.anterior').classList.add('oculto');
+    } else {
+      document.querySelector('.anterior').classList.remove('oculto');
+    }
+    if (siguienteURL === null) {
+      document.querySelector('.siguiente').classList.add('oculto');
+    } else {
+      document.querySelector('.siguiente').classList.remove('oculto');
+    }
+  } catch (error) {
     throw new Error(error);
   }
-  
 }
 
 function crearTarjeta(body, pokemon) {
@@ -62,89 +57,85 @@ function crearTarjeta(body, pokemon) {
 function borrarTarjetas() {
   const $tarjetas = document.querySelector('.pokemon-tarjetas');
   $tarjetas.innerHTML = '';
-  contadorPokemon = 0;
 }
 
 async function actualizarCartas(url) {
   try {
     llenarCartas(traerPokemons(url));
-  } catch(error) {
+  } catch (error) {
     throw new Error(error);
   }
-  
 }
 
 actualizarCartas(URL);
 
 document.querySelector('.siguiente').onclick = () => {
-
-  if (siguienteURL){
-  borrarTarjetas()
-  actualizarCartas(siguienteURL); 
+  if (siguienteURL) {
+    borrarTarjetas();
+    actualizarCartas(siguienteURL);
   }
-}
+};
 
 document.querySelector('.anterior').onclick = () => {
-
-  if(anteriorURL) {
-    borrarTarjetas()
+  if (anteriorURL) {
+    borrarTarjetas();
   }
   actualizarCartas(anteriorURL);
-}
+};
 
 document.querySelector('.logo-pokedex').onclick = () => {
-  borrarTarjetas()
+  borrarTarjetas();
   actualizarCartas(URL);
-}
+};
 
 Object.defineProperty(String.prototype, 'capitalize', {
-  value: function() {
+  value() {
     return this.charAt(0).toUpperCase() + this.slice(1);
   },
-  enumerable: false
+  enumerable: false,
 });
 
 function añadirInteraccion() {
   const $abrirPopup = document.querySelectorAll('.tarjeta');
   const $cerrarPopup = document.querySelector('.close-button');
   const $overlay = document.querySelector('#overlay');
-  const popup = document.querySelector('#popup')
+  const popup = document.querySelector('#popup');
 
-  $abrirPopup.forEach((tarjeta) =>{
+  $abrirPopup.forEach((tarjeta) => {
     tarjeta.onclick = (e) => {
       const dataPokemon = e.target.closest('[data-nombre-pokemon]');
       abrirPopup(popup);
       rellenarInfoPopup(dataPokemon.getAttribute('data-nombre-pokemon'));
-    }
-  })
+    };
+  });
 
   $cerrarPopup.onclick = () => {
     cerrarPopup(popup);
     borrarTipos();
-  }
- 
-  $overlay.addEventListener('click',() => {
+  };
+
+  $overlay.addEventListener('click', () => {
     const popup = document.querySelector('#popup.activo');
     cerrarPopup(popup);
     borrarTipos();
-  })
+  });
 
   function abrirPopup(popup) {
     const $body = document.querySelector('body');
     if (popup === null) return;
     popup.classList.add('activo');
-    $overlay.classList.add('activo'); 
+    $overlay.classList.add('activo');
 
-    $body.style.overflow='hidden';
+    $body.style.overflow = 'hidden';
   }
 
   function cerrarPopup(popup) {
     const $body = document.querySelector('body');
     if (popup === null) return;
     popup.classList.remove('activo');
-    $overlay.classList.remove('activo'); 
+    $overlay.classList.remove('activo');
 
-    $body.style.overflow='auto';
+    $body.style.overflow = 'auto';
   }
 
   function borrarTipos() {
@@ -156,54 +147,49 @@ function añadirInteraccion() {
 async function rellenarInfoPopup(nombrePokemon) {
   try {
     const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`);
-  const infoPokemon = await pokemon.json();
+    const infoPokemon = await pokemon.json();
 
-  const $tituloPopup = document.querySelector('.popup-header .title');
-  const $spritePokemon = document.querySelector('.popup-body .sprite-popup-pokemon');
-  const $peso = document.querySelector('.popup-body .peso');
-  const $estatura = document.querySelector('.popup-body .estatura');
+    const $tituloPopup = document.querySelector('.popup-header .title');
+    const $spritePokemon = document.querySelector('.popup-body .sprite-popup-pokemon');
+    const $peso = document.querySelector('.popup-body .peso');
+    const $estatura = document.querySelector('.popup-body .estatura');
 
-  $tituloPopup.textContent = infoPokemon.name.capitalize();
-  $spritePokemon.src = infoPokemon.sprites.front_default;
-  $peso.textContent = `${agregarComaAValores(infoPokemon.weight)} kg`;
-  $estatura.textContent = `${agregarComaAValores(infoPokemon.height)} m`;
+    $tituloPopup.textContent = infoPokemon.name.capitalize();
+    $spritePokemon.src = infoPokemon.sprites.front_default;
+    $peso.textContent = `${agregarComaAValores(infoPokemon.weight)} kg`;
+    $estatura.textContent = `${agregarComaAValores(infoPokemon.height)} m`;
 
-  if(infoPokemon.types.length > 1){
-    infoPokemon.types.forEach((e) => {
-      const $tipo = document.createElement('strong');      
+    if (infoPokemon.types.length > 1) {
+      infoPokemon.types.forEach((e) => {
+        const $tipo = document.createElement('strong');
+        const $listaTipos = document.querySelector('.popup-body .tipos-pokemon');
+
+        $tipo.classList.add('tipos');
+        $tipo.classList.add(`${e.type.name}`);
+        $tipo.textContent = e.type.name.capitalize();
+        $listaTipos.appendChild($tipo);
+      });
+    } else {
+      const $tipo = document.createElement('strong');
       const $listaTipos = document.querySelector('.popup-body .tipos-pokemon');
 
       $tipo.classList.add('tipos');
-      $tipo.classList.add(`${e.type.name}`);
-      $tipo.textContent = e.type.name.capitalize();
+      $tipo.classList.add(`${infoPokemon.types[0].type.name}`);
+      $tipo.textContent = infoPokemon.types[0].type.name.capitalize();
       $listaTipos.appendChild($tipo);
-    }) 
-  }
-  else {
-    const $tipo = document.createElement('strong')
-    const $listaTipos = document.querySelector('.popup-body .tipos-pokemon');
-
-    $tipo.classList.add('tipos');
-    $tipo.classList.add(`${infoPokemon.types[0].type.name}`);
-    $tipo.textContent = infoPokemon.types[0].type.name.capitalize();
-    $listaTipos.appendChild($tipo);
-
-  }
-  } catch(error){
+    }
+  } catch (error) {
     throw new Error(error);
   }
-  
 }
 
 function agregarComaAValores(num) {
   const string = num.toString();
-  if(string.length < 2) {
+  if (string.length < 2) {
     const resultado = `0${string.slice(0, -1)},${string.slice(-1)}`;
     return resultado;
   }
-  else {
-    const resultado = `${string.slice(0, -1)},${string.slice(-1)}`;
-    return resultado;
-  }
-}
 
+  const resultado = `${string.slice(0, -1)},${string.slice(-1)}`;
+  return resultado;
+}
